9ecd771fe822f51a2320de43f6bf1e674bce372e
dominic-myb <desguerra246@gmail.com>
2024-03-05 20:36:20 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include("includes/components/connection.php")?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+        $title = "Inventory Management System";
+        $bootstrap = "assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css";
+        $jQuery = "assets/js/vendor/query-3.7.1.min.js";
+        $mainJS = "assets/js/main.js";
+        $mainCSS = "assets/css/main.css";
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo $bootstrap?>">
+    <title>DCS <?php echo $title?></title>
+</head>
+<body>
+    
+
+
+    <script src="<?php echo $jQuery?>"></script>
+    <script src="<?php echo $mainJS?>"></script>
+</body>
+</html>
