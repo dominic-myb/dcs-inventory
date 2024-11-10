0164c1545e09f5e8da1c43822da614c672d077c1
Dominic Esguerra <desguerra246@gmail.com>
2024-11-10 20:33:51 +0800

    Modified Index

--- index.php
+++ index.php
@@ -1,24 +1,73 @@
-<?php include("includes/components/connection.php")?>
+<?php include("includes/components/connection.php") ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php 
-        $title = "Inventory Management System";
-        $bootstrap = "assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css";
-        $jQuery = "assets/js/vendor/query-3.7.1.min.js";
-        $mainJS = "assets/js/main.js";
-        $mainCSS = "assets/css/main.css";
+    <?php
+    $pageTitle = "Inventory Management System";
+    $BOOTSTRAP_PATH = "./assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css";
+    $MAIN_JS_PATH = "./assets/js/main.js";
+    $MAIN_CSS_PATH = "./assets/css/main.css";
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo $bootstrap?>">
-    <title>DCS <?php echo $title?></title>
+    <link rel="stylesheet" href="<?php echo $BOOTSTRAP_PATH ?>">
+    <link rel="stylesheet" href="<?php echo $MAIN_CSS_PATH ?>">
+    <title>DCS <?php echo $pageTitle ?></title>
 </head>
+
 <body>
-    
+    <main>
+        <div class="page-title">
+            <h1>DCS - Inventory Management System</h1>
+        </div>
+        <div class="content">
+            <table class="col-sm-3 col-lg-6 table-primary">
+                <thead>
+                    <tr>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Location</th>
+                        <th>Description</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
 
+                <tbody>
+                    <tr>
+                        <?php
+                        $query = "SELECT * FROM items";
+                        $res = $conn->query($query);
+                        if ($res->num_rows > 0) {
+                            while ($row = $res->fetch_assoc()) {
+                        ?>
+                                <td><?php echo $row['item_name'] ?></td>
+                                <td><?php echo $row['quantity'] ?></td>
+                                <td><?php echo $row['location'] ?></td>
+                                <td><?php echo $row['description'] ?></td>
+                                <td><?php echo $row['status'] ?></td>
+                                <td>
+                                    <a href="update.php?updateid=<?php echo $row['id'] ?>" class="btn btn-primary">Update</a>
+                                    <a href="delete.php?deleteid=<?php echo $row['id'] ?>" class="btn btn-danger">Delete</a>
+                                </td>
+                            <?php
+                            }
+                        } else {
+                            ?>
+                            <td colspan="6">0 Items Listed!</td>
+                        <?php
+                        }
 
-    <script src="<?php echo $jQuery?>"></script>
-    <script src="<?php echo $mainJS?>"></script>
+                        ?>
+                    </tr>
+                </tbody>
+
+            </table>
+        </div>
+
+    </main>
+    <script src="<?php echo $MAIN_JS_PATH ?>"></script>
 </body>
+
 </html>
