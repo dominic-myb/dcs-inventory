402cf41e65dc4d88b42e29fd4a59bb8879fc04a1
Dominic Esguerra <desguerra246@gmail.com>
2024-11-19 23:57:55 +0800

    Add Search Function

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("search-bar")
+    .addEventListener("keyup", async function () {
+      const query = this.value;
+
+      if (query.trim() === "") {
+        document.getElementById("table-body").innerHTML = "";
+        document.getElementById("search-result").style.display = "none";
+        return;
+      }
+
+      const response = await fetch(`backend/search.php?q=${query}`);
+      const data = await response.json();
+
+      const result = document.getElementById("table-body");
+      result.innerHTML = "";
+
+      if (data.length === 0) {
+        document.getElementById("search-result").style.display = "none";
+        result.innerHTML = `<tr class="row">
+                                <td colspan="6">0 Item Listed!</td>
+                            </tr>`;
+      } else {
+        document.getElementById("search-result").style.display = "block";
+        data.forEach((item) => {
+          result.innerHTML += `
+            <tr scope="row">
+                <td>${item.item_name}</td>
+                <td>${item.quantity}</td>
+                <td>${item.location}</td>
+                <td>${item.description}</td>
+                <td>${item.status}</td>
+                <td>
+                    <a href="update.php?update_id=${item.id}" class="btn btn-primary">Update</a>
+                    <a href="delete.php?delete_id=${item.id}" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+            `;
+        });
+      }
+    });
+});
