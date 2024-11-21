1ba8bc535e778366fa138338a070671b862cedce
Dominic Esguerra <desguerra246@gmail.com>
2024-11-21 23:07:16 +0800

    Added JQuery and Data Tables Path

--- layouts/index/scripts.php
+++ layouts/index/scripts.php
@@ -1,2 +1,4 @@
 <script src="<?= $BOOTSTRAP_JS_PATH ?>"></script>
+<script src="<?= $JQUERY_PATH ?>"></script>
+<script src="<?= $DATA_TABLES_JS_PATH ?>"></script>
 <script src="<?= $MAIN_JS_PATH ?>"></script>
