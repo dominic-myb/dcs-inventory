6af87ee884f139890853cdf4e0c905c8d21c6fda
Dominic Esguerra <desguerra246@gmail.com>
2024-11-21 23:05:45 +0800

    Added doctype and html opening tag

--- layouts/index/head.php
+++ layouts/index/head.php
@@ -1,7 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $BOOTSTRAP_CSS_PATH ?>">
+    <link rel="stylesheet" href="<?= $DATA_TABLE_CSS_PATH ?>">
     <link rel="stylesheet" href="<?= $MAIN_CSS_PATH ?>">
-    <title><?= $pageTitle ?></title>
+    <title><?= $PAGE_TITLE ?></title>
 </head>
