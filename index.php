bdce9eaf7ff33ed08489231f34b60a9659d81dc2
Dominic Esguerra <desguerra246@gmail.com>
2024-11-21 23:04:41 +0800

    Sort out Index HTML

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php include("./includes/components/connection.php") ?>
-<!DOCTYPE html>
-<html lang="en">
-
 <?php
-$pageTitle = "Inventory Management System";
+include("./includes/components/connection.php");
+$PAGE_TITLE = "Inventory Management System";
 $BOOTSTRAP_CSS_PATH = "./assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css";
+$DATA_TABLES_CSS_PATH = "//cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css";
 $MAIN_CSS_PATH = "./assets/css/main.css";
+$JQUERY_PATH = "./assets/js/vendor/jquery-3.7.1.min.js";
 $BOOTSTRAP_JS_PATH = "./assets/css/vendor/bootstrap-5.2.3-dist/js/bootstrap.min.js";
+$DATA_TABLES_JS_PATH = "//cdn.datatables.net/2.1.8/js/dataTables.min.js";
 $MAIN_JS_PATH = "./assets/js/main.js";
 include("./layouts/index/head.php");
 ?>
