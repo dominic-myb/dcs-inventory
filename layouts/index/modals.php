f7eb1caf4f98c9d81cfcdd43ed64de1dfabdf287
Dominic Esguerra <desguerra246@gmail.com>
2024-11-21 23:06:21 +0800

    Made code more readable

--- layouts/index/modals.php
+++ layouts/index/modals.php
@@ -3,10 +3,12 @@
 <div id="add-item-modal" class="modal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
+
             <div class="modal-header">
                 <h5 class="modal-title">Add Item</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+
             <div class="modal-body">
                 <form id="add-item-form" method="post">
 
@@ -27,10 +29,12 @@
 
                 </form>
             </div>
+
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                 <button type="button" class="btn btn-primary">Save</button>
             </div>
+
         </div>
     </div>
 </div>
