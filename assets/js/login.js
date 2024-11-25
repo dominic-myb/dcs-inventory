e38d877b4c88e6aa6be70cf2a30101f8c6ea2970
Dominic Esguerra <desguerra246@gmail.com>
2024-11-25 19:33:40 +0800

    Added Password Show and Hide Script

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /***** SHOW AND HIDE OF PASSWORD *****/
+  const passToggle = document.getElementById("pass-toggle");
+  const passInput = document.getElementById("pass-input");
+  passToggle.addEventListener("click", () => {
+    passInput.style.margin = 0;
+    if (passInput.type === "password") {
+      passInput.type = "text";
+      passToggle.value = "Hide";
+    } else {
+      passInput.type = "password";
+      passToggle.value = "Show";
+    }
+  });
+
+  /***** FORM SUBMISSION *****/
   document
     .getElementById("login-form")
     .addEventListener("submit", async (e) => {
@@ -22,3 +37,14 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch((error) => console.error("Error:", error));
     });
 });
+
+/***** DETECT IF INCORRECT PASS IS SHOWN *****/
+
+const incorrectPassMsg = document.getElementById("pass-incorrect");
+const submitBtn = document.getElementById("submit-btn");
+// incorrectPassMsg.hasAttribute;
+if (incorrectPassMsg.hasAttribute("display") === "flex") {
+  submitBtn.style.marginTop = 0;
+} else {
+  submitBtn.style.marginTop = "1rem";
+}
