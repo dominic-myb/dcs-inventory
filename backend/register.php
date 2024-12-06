ee372e6ce04b0b785875fdd3351c5557f54d7ec1
Dominic Esguerra <desguerra246@gmail.com>
2024-12-06 21:42:51 +0800

    -update register php backend

--- backend/register.php
+++ backend/register.php
@@ -12,7 +12,30 @@ $password = hash('sha256', $password);
 $is_admin = 0;
 
 include("functions.php");
-addNewAccount($conn, $username, $password, $firstname, $lastname, $is_admin);
+if (!isUsernameAvailable($conn, $username)) {
+  echo json_encode([
+    "status" => "error",
+    "message" => "Username not Available!"
+  ]);
+  exit();
+}
+
+$sql = "INSERT INTO accounts (username, hashed_password, first_name, last_name, is_admin) VALUES (?,?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssi", $username, $password, $firstname, $lastname, $is_admin);
+
+if (!$stmt->execute()) {
+  echo json_encode([
+    "status" => "error",
+    "message" => "Account not Registered!"
+  ]);
+  $stmt->close();
+  $conn->close();
+}
+echo json_encode([
+  "status" => "success",
+  "message" => "Account Registered!"
+]);
 
 $stmt->close();
 $conn->close();
