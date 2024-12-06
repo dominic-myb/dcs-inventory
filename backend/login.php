33c75c32a8e0e3143ed57badae7c75c8fe4e4369
Dominic Esguerra <desguerra246@gmail.com>
2024-12-06 21:42:33 +0800

    -update login php backend

--- backend/login.php
+++ backend/login.php
@@ -8,8 +8,24 @@ $username = $data->username ?? '';
 $password = $data->password ?? '';
 $password = hash('sha256', $password);
 
-include("functions.php");
-authenticateAccount($conn, $username, $password);
+$sql = "SELECT username, hashed_password FROM accounts WHERE username=? and hashed_password=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+$res = $stmt->get_result();
+
+if($res->num_rows === 0){
+  echo json_encode([
+    "status" => "error",
+    "message" => "Invalid Username or Password"
+  ]);
+  exit();
+}
+echo json_encode([
+  "status" => "success",
+  "message" => "Login Successful!"
+]);
+
 
 $stmt->close();
 $conn->close();
