8a7a433ebfa26485be492c68acc1d7d49591f985
Dominic Esguerra <desguerra246@gmail.com>
2024-12-06 21:42:11 +0800

    -check_username initial commit

---
+++ backend/check_username.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+include("db_config.php");
+
+$data = json_decode(file_get_contents('php://input'));
+
+$username = $data->username ?? '';
+
+$sql = "SELECT username FROM accounts WHERE username = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+if (!$result = $stmt->get_result()){
+  json_encode([
+    "status" => "success",
+    "message" => "Username is valid!"
+  ]);
+  exit();
+}
+json_encode([
+  "status" => "error",
+  "message" => "Username is unavailable!"
+]);
+
+$stmt->close();
+$conn->close();
