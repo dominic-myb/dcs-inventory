9c85405fe43f7f47c72878762132e9214f24ef4f
Dominic Esguerra <desguerra246@gmail.com>
2024-12-07 19:19:08 +0800

    -added password strength indicator

--- assets/js/register.js
+++ assets/js/register.js
@@ -15,18 +15,19 @@
   RED: #FF6961
   ORANGE: #FFB54C
   GREEN: #7ABD7E
-  */
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
-  function showErrorMsg(errorMsgId, msg, seconds) {
+  function showErrorMsg(id, msg, time) {
     const pTag = document.createElement("p");
     pTag.textContent = msg;
-    errorMsgId.appendChild(pTag);
-    errorMsgId.style.display = "flex";
+    id.appendChild(pTag);
+    id.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
+
     setTimeout(() => {
-      errorMsgId.style.display = "none";
+      id.style.backgroundColor = "transparent";
       pTag.textContent = "";
-    }, seconds);
+    }, time);
   }
 
   // ?MAKE THIS RETURNS INTEGER?
@@ -36,118 +37,91 @@ document.addEventListener("DOMContentLoaded", () => {
     const hasUppercase = /[A-Z]/.test(password);
     const hasNumber = /\d/.test(password);
     const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const isLongEnough = password.length >= 12;
+    if (!standardLength) return 0;
+    const strengthScore = [hasLowercase, hasUppercase, hasNumber, hasSymbol, isLongEnough].filter(Boolean).length;
 
-    if (!standardLength) return "Weak";
-    const strengthScore = [
-      hasLowercase,
-      hasUppercase,
-      hasNumber,
-      hasSymbol,
-    ].filter(Boolean).length;
-    if (strengthScore === 4 && password.length >= 12) return "Strong";
-    if (strengthScore >= 2) return "Moderate";
-
-    return "Weak";
-  }
-
-  //? REMOVE THIS? OR CONTINUE? IF CONTINUE GET A TXT FILE OF PWNED PASSWORDS THEN CONVERT INTO HASH THEN CONVERT TO JSON
-  async function isPasswordPwned(password) {
-    const sha1 = new Hashes.SHA1().hex(password);
-    const prefix = sha1.substring(0, 5);
-    const suffix = sha1.substring(5);
-
-    const response = await fetch(
-      `https://api.pwnedpasswords.com/range/${prefix}`
-    );
-    const data = await response.text();
-
-    return data.includes(suffix);
+    return strengthScore;
   }
 
   document.getElementById("passInput").addEventListener("keyup", async (e) => {
     e.preventDefault();
     const password = document.getElementById("passInput").value.trim();
-    const passwordStrength = passwordStrengthChecker(password);
-    // console.log(`Password Strength: ${passwordStrength}`);
+    const passStrengthMeterId = document.getElementById("passStrength");
+    const strengthScore = passwordStrengthChecker(password);
+    passStrengthMeterId.value = strengthScore;
+
+    const passStrengthTextId = document.getElementById("passStrengthText");
+
+    if (strengthScore >= 5) passStrengthTextId.textContent = "Strong";
+    else if (strengthScore >= 3) passStrengthTextId.textContent = "Moderate";
+    else passStrengthTextId.textContent = "Weak";
   });
 
-  document
-    .getElementById("registration-form")
-    .addEventListener("submit", async (e) => {
-      e.preventDefault();
+  document.getElementById("registrationForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-      const submitBtn = document.getElementById("submitBtn");
-      submitBtn.disabled = true;
+    const submitBtn = document.getElementById("submitBtn");
+    submitBtn.disabled = true;
 
-      setTimeout(() => {
-        submitBtn.disabled = false;
-      }, 3000);
+    setTimeout(() => {
+      submitBtn.disabled = false;
+    }, 3000);
 
-      const username = DOMPurify.sanitize(
-        document.getElementById("userInput").value.trim()
-      );
-      const password = DOMPurify.sanitize(
-        document.getElementById("passInput").value.trim()
-      );
-      const passInputConfirm = DOMPurify.sanitize(
-        document.getElementById("passInputConfirm").value.trim()
-      );
+    const username = DOMPurify.sanitize(document.getElementById("userInput").value.trim());
+    const password = DOMPurify.sanitize(document.getElementById("passInput").value.trim());
+    const passInputConfirm = DOMPurify.sanitize(document.getElementById("passInputConfirm").value.trim());
 
-      const userErrorMsg = document.getElementById("userErrorMsg");
-      const passErrorMsg = document.getElementById("passErrorMsg");
-      const seconds = 3000;
-      
-      if (username.length < 8){
-        let msg = "Username must be at least 8 characters long.";
-        showErrorMsg(userErrorMsg, msg, 3000);
-        return;
+    const errorMsgId = document.getElementById("errorMsg");
+    const seconds = 3000;
+
+    if (username.length < 8) {
+      let msg = "Minimum length: 8 characters";
+      showErrorMsg(errorMsgId, msg, 3000);
+      return;
+    }
+
+    if (!/^[a-z_0-9]+$/.test(username)) {
+      let msg = "Only a-z, 0-9, or _ are allowed.";
+      showErrorMsg(errorMsgId, msg, seconds);
+      return;
+    }
+
+    if (password !== passInputConfirm) {
+      let msg = "Password doesn't match";
+      showErrorMsg(errorMsgId, msg, 3000);
+      return;
+    }
+
+    const firstname = DOMPurify.sanitize(document.getElementById("fnameInput").value.trim());
+
+    const lastname = DOMPurify.sanitize(document.getElementById("lnameInput").value.trim());
+
+    try {
+      const response = await fetch("../backend/register.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ firstname, lastname, username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      if (!/^[a-z_0-9]+$/.test(username)){
-        let msg = "Username must only have a-z, 0-9 and underscore";
-        showErrorMsg(userErrorMsg, msg, seconds);
-        return;
+      const data = await response.json();
+
+      if (data.status === "success") {
+        console.log("Registration Successful:", data.message);
+        window.location.href = "login.php";
+      } else {
+        console.error("Registration failed:", data.message);
+        showErrorMsg(passErrorMsg, data.message, 3000);
       }
-      
-      if (password !== passInputConfirm) {
-        let msg = "Password doesn't match";
-        showErrorMsg(passErrorMsg, msg, 3000);
-        return;
-      }
-
-      const firstname = DOMPurify.sanitize(
-        document.getElementById("fnameInput").value.trim()
-      );
-
-      const lastname = DOMPurify.sanitize(
-        document.getElementById("lnameInput").value.trim()
-      );
-
-      try {
-        const response = await fetch("../backend/register.php", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ firstname, lastname, username, password }),
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        if (data.status === "success") {
-          console.log("Registration Successful:", data.message);
-          window.location.href = "login.php";
-        } else {
-          console.error("Registration failed:", data.message);
-          showErrorMsg(passErrorMsg, data.message, 3000);
-        }
-      } catch (error) {
-        console.error("An error occurred:", error);
-        showErrorMsg(passErrorMsg, error, 3000);
-      }
-    });
+    } catch (error) {
+      console.error("An error occurred:", error);
+      showErrorMsg(passErrorMsg, error, 3000);
+    }
+  });
 });
