e2c6b12dd20592926ed86a04875516c3da987ea8
Dominic Esguerra <desguerra246@gmail.com>
2024-12-12 17:26:56 +0800

    -update check username php

--- backend/check_username.php
+++ backend/check_username.php
@@ -3,24 +3,36 @@ header('Content-Type: application/json');
 include("db_config.php");
 
 $data = json_decode(file_get_contents('php://input'));
+$username = $conn->real_escape_string($data->username ?? '');
 
-$username = $data->username ?? '';
+try {
+  $sql = "SELECT username FROM accounts WHERE username = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("s", $username);
+  $stmt->execute();
+  $stmt->store_result();
 
-$sql = "SELECT username FROM accounts WHERE username = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $username);
-$stmt->execute();
-if (!$result = $stmt->get_result()){
+  if ($stmt->num_rows > 0) {
+    http_response_code(409);
+    echo json_encode([
+      "status" => "error",
+      "message" => "Username is unavailable!"
+    ]);
+    exit();
+  } else {
+    http_response_code(200);
+    echo json_encode([
+      "status" => "success",
+      "message" => "Username is valid!"
+    ]);
+  }
+} catch (Exception $e) {
+  http_response_code(500);
   echo json_encode([
-    "status" => "success",
-    "message" => "Username is valid!"
+    "status" => "error",
+    "message" => $e->getMessage(),
   ]);
-  exit();
 }
-echo json_encode([
-  "status" => "error",
-  "message" => "Username is unavailable!"
-]);
 
 $stmt->close();
 $conn->close();
