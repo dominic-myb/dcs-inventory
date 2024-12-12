e1651b3ed3b0e1a761adea9f43bb3ca294545620
Dominic Esguerra <desguerra246@gmail.com>
2024-12-12 13:47:07 +0800

    -update db_config

--- backend/db_config.php
+++ backend/db_config.php
@@ -4,6 +4,8 @@ $DB_USER = "root";
 $DB_PASS = "";
 $DB_NAME = "dcs_db";
 
-if (!$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME)) {
-  die("Connection Failed: " . mysqli_connect_error());
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
