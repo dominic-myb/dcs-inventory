747f9a9fbab6803d75275a39a36cfdb224f56dfc
Dominic Esguerra <desguerra246@gmail.com>
2024-12-13 20:36:59 +0800

    -update: error handling fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -32,24 +32,25 @@ document.addEventListener("DOMContentLoaded", () => {
   /***** FORM SUBMISSION *****/
   document.getElementById("login-form").addEventListener("submit", async (e) => {
     e.preventDefault();
-
-    const submitBtn = document.getElementById("submitBtn");
-
-    submitBtn.disabled = true;
-    setTimeout(() => {
-      submitBtn.disabled = false;
-    }, 3000);
-
-    const username = DOMPurify.sanitize(document.getElementById("userInput").value.trim());
-    const password = DOMPurify.sanitize(document.getElementById("passInput").value.trim());
-    const token = document.getElementById("csrfToken").value;
-    const errorMsg = document.getElementById("errorMsg");
-    const formData = {
-      username: username,
-      password: password,
-      csrf_token: token,
-    };
     try {
+      const submitBtn = document.getElementById("submitBtn");
+      const errorMsg = document.getElementById("errorMsg");
+      if (!errorMsg || !submitBtn) throw new Error("Reference Id not found.");
+
+      submitBtn.disabled = true;
+      setTimeout(() => {
+        submitBtn.disabled = false;
+      }, 3000);
+
+      const username = DOMPurify.sanitize(document.getElementById("userInput").value.trim());
+      const password = DOMPurify.sanitize(document.getElementById("passInput").value.trim());
+      const token = document.getElementById("csrfToken").value;
+      const formData = {
+        username: username,
+        password: password,
+        csrf_token: token,
+      };
+      
       const response = await fetch("../backend/login.php", {
         method: "POST",
         headers: {
@@ -58,18 +59,18 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify(formData),
       });
-      
+
       const data = await response.json();
 
       if (data.status === "success") {
-        console.log("Login successful:", data.message);
+        console.log(`Login successful: ${data.message}`);
         window.location.href = "../index.php";
       } else {
-        console.error("Login failed:", data.message);
+        console.error(`Login failed: ${data.message}`);
         showErrorMsg(errorMsg, data.message, 3000);
       }
     } catch (error) {
-      console.error("An error occurred:", error);
+      console.error(`An error occurred: ${error}`);
     }
   });
   /***** END *****/
