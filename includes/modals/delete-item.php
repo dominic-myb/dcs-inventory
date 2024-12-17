7c5ef2375b80e9e061f6be52df81c5dd85a6904c
Dominic Esguerra <desguerra246@gmail.com>
2024-12-17 19:14:47 +0800

    -added delete modal

---
+++ includes/modals/delete-item.php
@@ -0,0 +1,24 @@
+<div id="deleteItemModal" class="modal delete-item-modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form id="deleteItemForm">
+
+        <div class="modal-header">
+          <h5 class="modal-title">Delete Item</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body">
+          <input type="hidden" name="itemId" id="itemId">
+          <p>Are you sure that you want to delete, <span id="itemNameDisplay"></span>?</p>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary" id="saveBtn">Save</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
