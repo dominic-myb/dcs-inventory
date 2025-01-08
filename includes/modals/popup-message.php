6fa9a9558fcdfdfd65b48675a71a208c602be22d
Dominic Esguerra <desguerra246@gmail.com>
2025-01-08 17:00:35 +0800

    -added popup message php file

---
+++ includes/modals/popup-message.php
@@ -0,0 +1,15 @@
+<div id="popupMsg" class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title" id="popupTitle"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <div class="modal-body" id="popupMsgContent">
+
+      </div>
+    </div>
+  </div>
+</div>
